3bc5950ef2a9e19eb74ec0c95446123f4cd74841
KoreaMoney <kimdowon1404@gmail.com>
2024-11-13 14:45:06 +0900

    feat : 3d 코드 개선

--- src/components/OfficeThree.jsx
+++ src/components/OfficeThree.jsx
@@ -24,45 +24,44 @@ const OfficeThree = () => {
   const cameraRef = useRef(null);
   const rendererRef = useRef(null);
   const controlsRef = useRef(null);
-
   const sceneRef = useRef(new THREE.Scene());
 
-  /**
-   * 3D
-   */
   // SIZES
   const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
   };
+
   // CAMERA
-  const cameraSetting = () => {
+  const setupCamera = () => {
     cameraRef.current = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 2000);
-    cameraRef.current.position.set(-3.684167226778625, 13.703704502199631, -19.71748398597716);
+    cameraRef.current.position.set(-3.684, 13.704, -19.717);
     sceneRef.current.add(cameraRef.current);
   };
+
   // CONTROL
-  const controlSetting = () => {
+  const setupControls = () => {
     controlsRef.current = new OrbitControls(cameraRef.current, canvasRef.current);
-    controlsRef.current.target.set(-3.83965147511199, 1.0627257131879073, -7.063586155882121);
-    controlsRef.current.enableDamping = true;
-    controlsRef.current.maxPolarAngle = Math.PI / 2; // 최대 세로 각도 제한 (90도)
-    controlsRef.current.minPolarAngle = 0; // 최소 세로 각도 제한
+    controlsRef.current.target.set(-3.84, 1.063, -7.064);
+    controlsRef.current.enableDamping = false;
+    controlsRef.current.maxPolarAngle = Math.PI / 2;
+    controlsRef.current.minPolarAngle = 0;
   };
-  // Light
-  const lightSetting = () => {
+
+  // LIGHT
+  const setupLights = () => {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     sceneRef.current.add(ambientLight);
 
     const directionalLight = new THREE.DirectionalLight(0xfdf4dc, 1.2);
     directionalLight.position.set(1.4, 2.65, 2.65);
-    directionalLight.castShadow = true;
     sceneRef.current.add(directionalLight);
 
     const fillLight = new THREE.DirectionalLight(0xb6ceff, 0.3);
     fillLight.position.set(-2, 1, -1);
     sceneRef.current.add(fillLight);
   };
+
   // SCENE CREATE
   const createScene = () => {
     const canvas = canvasRef.current;
@@ -72,17 +71,16 @@ const OfficeThree = () => {
       alpha: true,
     });
     rendererRef.current.setSize(sizes.width, sizes.height);
-    rendererRef.current.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    rendererRef.current.setPixelRatio(Math.min(window.devicePixelRatio, 1)); // 성능 최적화를 위해 1로 설정
     rendererRef.current.setClearColor(0x292929, 0);
-    rendererRef.current.sortObjects = false;
-    rendererRef.current.colorSpace = THREE.SRGBColorSpace;
     rendererRef.current.toneMapping = THREE.ACESFilmicToneMapping;
     rendererRef.current.toneMappingExposure = 1;
 
-    cameraSetting();
-    controlSetting();
-    lightSetting();
+    setupCamera();
+    setupControls();
+    setupLights();
   };
+
   // RESIZE EVENT
   const onWindowResize = () => {
     sizes.width = window.innerWidth;
@@ -95,12 +93,11 @@ const OfficeThree = () => {
 
     if (rendererRef.current) {
       rendererRef.current.setSize(sizes.width, sizes.height);
-      rendererRef.current.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     }
   };
 
   // LOAD
-  const modelLoader = () => {
+  const loadModel = () => {
     const loader = new GLTFLoader();
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.6/");
@@ -109,28 +106,27 @@ const OfficeThree = () => {
     new RGBELoader().load(hdr, (texture) => {
       texture.mapping = THREE.EquirectangularReflectionMapping;
       sceneRef.current.environment = texture;
-      loader.load(
-        model,
-        (gltf) => {
-          modelRef.current = gltf.scene;
-          gltf.scene.scale.set(1, 1, 1);
-          gltf.scene.position.set(0, 0, 0);
-
-          gltf.scene.traverse((node) => {
-            const ceiling = node.name.includes("ceiling");
-            if (ceiling) {
-              node.visible = false;
-            }
-          });
-
-          sceneRef.current.add(gltf.scene);
-        },
-        undefined,
-        (error) => {
-          console.error("An error happened during loading:", error);
-        }
-      );
     });
+
+    loader.load(
+      model,
+      (gltf) => {
+        modelRef.current = gltf.scene;
+        gltf.scene.scale.set(1, 1, 1);
+        gltf.scene.position.set(0, 0, 0);
+
+        gltf.scene.traverse((node) => {
+          if (node.name.includes("ceiling")) {
+            node.visible = false;
+          }
+        });
+        sceneRef.current.add(gltf.scene);
+      },
+      undefined,
+      (error) => {
+        console.error("An error happened during loading:", error);
+      }
+    );
   };
 
   // ANIMATION
@@ -149,46 +145,22 @@ const OfficeThree = () => {
    * CLEAN-UP
    */
   useEffect(() => {
-    const setupScene = () => {
-      createScene();
-      modelLoader();
-      animate();
-    };
-
-    const setupEventListeners = () => {
-      const handleResize = throttle(onWindowResize, 500);
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    };
+    createScene();
+    loadModel();
+    animate();
 
     const cleanupScene = () => {
       if (rendererRef.current) rendererRef.current.dispose();
       if (sceneRef.current) sceneRef.current.clear();
     };
 
-    setupScene();
+    const handleResize = throttle(onWindowResize, 500);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      setupEventListeners();
       cleanupScene();
+      window.removeEventListener("resize", handleResize);
     };
-  }, []);
-
-  /**
-   * DEBUGS
-   */
-  useEffect(() => {
-    document.addEventListener("keydown", (event) => {
-      switch (event.code) {
-        case "Space":
-          console.log("------------------------");
-          console.log(cameraRef.current.position);
-          console.log(controlsRef.current.target);
-          break;
-      }
-    });
   }, []);
 
   return (
