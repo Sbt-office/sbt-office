02f88573a5f656a1572787e5afda02fb4b72adcb
lunenia <qudrnjs96@naver.com>
2024-11-20 08:34:27 +0900

    a

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-console.log("test");
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
