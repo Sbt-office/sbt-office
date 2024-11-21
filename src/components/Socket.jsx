a6aeda31c1975d64b5dd4d1dc9e53c8bd7d20bcb
lunenia <qudrnjs96@naver.com>
2024-11-21 14:06:02 +0900

    socket 개발서버 테스트

--- src/components/Socket.jsx
+++ src/components/Socket.jsx
@@ -7,6 +7,38 @@ const Socket = () => {
 
   const { setData, setIsConnected } = useSocketStore();
 
+  // useEffect(() => {
+  //   const socketConnection = async () => {
+  //     socketRef.current = await io.connect("http://192.168.0.75:3000", { transports: ["websocket"] });
+  //     socketRef.current.on("kafkaMessage", (data) => {
+  //       console.log(data);
+  //       // setData(data);
+  //     });
+
+  //     socketRef.current.on("connect", () => {
+  //       console.log("connect");
+  //       // setIsConnected(true);
+  //     });
+
+  //     socketRef.current.on("disconnect", () => {
+  //       console.log("disconnect");
+  //       socketRef.current = null;
+  //       // setIsConnected(false);
+  //     });
+  //   };
+
+  //   if (!socketRef.current) {
+  //     socketConnection();
+  //   }
+
+  //   return () => {
+  //     if (socketRef.current) {
+  //       socketRef.current.disconnect();
+  //       socketRef.current = null;
+  //     }
+  //   };
+  // }, []);
+
   useEffect(() => {
     const socketConnection = async () => {
       socketRef.current = await io.connect(window.location.origin);
@@ -22,6 +54,7 @@ const Socket = () => {
       socketRef.current.on("disconnect", () => {
         console.log("disconnect");
         setIsConnected(false);
+        socketRef.current = null;
       });
     };
 
